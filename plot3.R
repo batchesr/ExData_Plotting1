fa8be7785360192133362f715099a7af25d4ec0b
Samantha <46547820+batchesr@users.noreply.github.com>
2019-03-11 21:19:09 -0400

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -20,13 +20,13 @@ data$Datetime <- as.POSIXct(Datetime)
 
 #Create the plot
 with(data, {
-        plot(Sub_metering_1~Datetime, col="black", type="l", ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Sub_metering_1~Datetime, col="black", type="l", ylab="Energy sub metering", xlab="")
                      lines(Sub_metering_2~Datetime, col="red")
                      lines(Sub_metering_3~Datetime, col="blue")
 })
 
 #Add a legend
-legend("topright", col=c("black","red","blue"), lty=1, lwd=2, legend=c("Sub metering 1", "Sub metering 2", "Sub metering 3"))
+legend("topright", col=c("black","red","blue"), lty=1, lwd=2, xpd=TRUE, legend=c("Sub metering 1", "Sub metering 2", "Sub metering 3"))
 
 #Create png file
 png("plot3.png",width=480,height=480,units="px")
